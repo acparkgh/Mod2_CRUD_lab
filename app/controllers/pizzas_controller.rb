f0a7944bc1963277567b8318a4708bf3bde30a8e
acparkgh <acpark123@gmail.com>
2020-08-07 16:17:44 -0400

    added update

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -7,14 +7,11 @@ class PizzasController < ApplicationController
 
   def homepage
     render(:homepage)
-  
   end
-
+  
   def show
-    
-    @pizza = Pizza.find(params[:id])
+    @pizza = Pizza.find(params[:id])  # @pizza = Pizza.find(10)
     render(:show)
-
   end
 
   def new
@@ -27,9 +24,28 @@ class PizzasController < ApplicationController
     price = params["pizza"]["price"]
     pizzatype = params["pizza"]["pizzatype"]
     pizza = Pizza.create({name: name, price: price, pizzatype: pizzatype})
-    redirect_to pizzas_path
+    redirect_to(pizza_path(pizza.id))
   end
 
+  def edit
+    @pizza = Pizza.find(params[:id])
+  end
+
+  def update
+    @pizza = Pizza.find(params[:id])
+    name = params["pizza"]["name"]
+    price = params["pizza"]["price"]
+    pizzatype = params["pizza"]["pizzatype"]
+    @pizza.update({name: name, price: price, pizzatype: pizzatype})
+    redirect_to pizza_path(@pizza.id)
+  end
+    
+  
+
+    # @article = Article.find(params[:id])
+    # @article.update(title: params[:article][:title], description: params[:article][:description])
+    # redirect_to article_path(@article)
+  
      
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get("/", {to: "pizzas#homepage", as: "whatever"})
-
   get("/pizzas", {to: "pizzas#index"})
-  
   get("/pizzas/new", {to: "pizzas#new"})
-  
-  post("/pizzas", {to: "pizzas#create", as: "addpizza"})
   get("/pizzas/:id", {to: "pizzas#show", as: "pizza"})
-
+  post("/pizzas", {to: "pizzas#create", as: "addpizza"})
+  get("/pizzas/:id/edit", {to: "pizzas#edit"})
+  patch("/pizzas/:id", {to: "pizzas#update"})
 
 end
